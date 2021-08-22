cc46663d24b0ebc2aaa688c4798fd80de9bae90e
lucasfarmwald <farmwaldlucas@gmail.com>
2021-08-21 22:37:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers([name1,name2,name3,name4]){
+    return [name1,name2]
+}
+function returnLastTwoDrivers([name1,name2,name3,name4]){
+       return [name3,name4]
+}
+
+
+function selectingDrivers([returnFirstTwoDrivers , returnLastTwoDrive ]){
+    return [returnFirstTwoDrivers,returnLastTwoDrive]
+}
